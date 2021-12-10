8e69731fc90c26c2693e3a6f04a77b35c7af44e7
Happytiger-exe <61087988+Happytiger-exe@users.noreply.github.com>
2021-12-10 15:49:05 +0200

    Add files via upload

---
+++ Test la C-Sharp/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Test_la_C_Sharp
+{
+    partial class Sawcon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(30, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(144, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 51);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numbers";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SUM",
+            "REVERSE",
+            "MAX",
+            "BASE 2"});
+            this.comboBox1.Location = new System.Drawing.Point(309, 208);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(155, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.textBox1.Location = new System.Drawing.Point(309, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(24, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 36);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Introduce number";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(24, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(230, 36);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Select operation";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(309, 316);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(155, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(302, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 40);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Result";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(496, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "_____________________________________________________________";
+            // 
+            // Sawcon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(496, 378);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Sawcon";
+            this.Text = "Sawcon";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Test la C-Sharp/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_la_C_Sharp
+{
+    public partial class Sawcon : Form
+    {
+        public string operation;
+        public int number;
+
+        private int Sum(int num)
+        {
+            int s = 0;
+            while(num != 0)
+            {
+                s += num % 10;
+                num /= 10;
+            }
+            return s;
+        }
+
+        private int Rev(int num)
+        {
+            int n = 0;
+            while(num != 0)
+            {
+                n += num % 10;
+                n *= 10;
+                num /= 10;
+            }
+            return n / 10;
+        }
+
+        private string BaseTwo(int num)
+        {
+            return Convert.ToString(num, 2);
+        }
+
+        private int Max(int num)
+        {
+            int cmax = 0;
+
+            while(num != 0)
+            {
+                if (cmax < num % 10) cmax = num % 10;
+                num /= 10;
+            }
+            return cmax;
+        }
+
+        public Sawcon()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            operation = comboBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number = int.Parse(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                MessageBox.Show("Introduce your number \n");
+            }
+
+            if (operation != null)
+            {
+                switch (operation)
+                {
+                    case "SUM": textBox2.Text = Convert.ToString(Sum(number));
+                        break;
+                    case "MAX": textBox2.Text = Convert.ToString(Max(number));
+                        break;
+                    case "REVERSE": textBox2.Text = Convert.ToString(Rev(number));
+                        break;
+                    case "BASE 2": textBox2.Text = Convert.ToString(BaseTwo(number));
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select operation");        
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
